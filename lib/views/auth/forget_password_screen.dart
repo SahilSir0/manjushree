27e10d152523d267775d8d6a192784d0f0e2eb24
SahilSir0 <sahilgubhaju392@gmail>
2024-11-24 13:15:35 +0545

    Forgetpw

--- lib/views/auth/forget_password_screen.dart
+++ lib/views/auth/forget_password_screen.dart
@@ -1,27 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:manjushree/controller/auth/login_controller.dart';
+import 'package:manjushree/utils/custom_text_style.dart';
+
+import '../../utils/validatior.dart';
+import '../../widgets/custom/custom_textfield.dart';
 
 class ForgetPasswordScreen extends StatelessWidget {
-  const ForgetPasswordScreen({super.key});
+  ForgetPasswordScreen({super.key});
+  final c = Get.put(LogInController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: SizedBox(
-            height: 500,
-            width: 200,
-            child: Column(
-              children: [
-                Image(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+                height: 200,
+                width: 500,
+                child: Image(
                   image: AssetImage("assets/common/forgetpassword.png"),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                      "Please enter your email address to receive a verification code"),
-                )
-              ],
-            )),
+                )),
+            Padding(
+              padding: const EdgeInsets.all(50.0),
+              child: Center(
+                child: Text(
+                    "Please enter your email address to receive a verification code",
+                    textAlign: TextAlign.center,
+                    style: CustomTextStyles.f12W600()),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: CustomTextField(
+                preIconPath: Icons.person_outline_sharp,
+                hint: "Email",
+                validator: Validators.checkEmailField,
+                controller: c.emailController,
+                textInputAction: TextInputAction.next,
+                textInputType: TextInputType.emailAddress,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
